c4b6f72a447f4ea1a36de13d7a0fcc7a28018ca8
pawn-4-git <pawn4sns+dummy@gmail.com>
2025-05-04 15:17:29 +0900

    修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
+      "@next/next/no-html-link-for-pages": "warn"
     },
   }),
 ];

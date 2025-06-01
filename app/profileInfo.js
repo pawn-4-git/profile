d6f2ac4c5f72b7627ff508c2d0fa51ffa472aacb
pawn-4-git <pawn4sns+dummy@gmail.com>
2025-06-01 22:54:49 +0900

    画面の表示の修正

---
+++ app/profileInfo.js
@@ -0,0 +1,12 @@
+const profileInfo = {
+    name: "蓬莱 寿成",
+    title: "",
+    introduction: "",
+    contact: {
+        github: "https://github.com/pawn-4-git",
+        twitter: "https://x.com/pawn_4_t",
+        SpeakerDeck: "https://speakerdeck.com/pawn_4_s"
+    }
+};
+
+export default profileInfo;
